24753444634cee013dbeb3fbe825d56edb43e761
Unknown <tristan@elantris.com>
2019-02-10 12:29:32 -0800

    Some Mobile

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,18 @@
-$(function(){
-   $("a.dropdown").click(function(){
-      var filter = $(this).data("filter"); 
-       
-       $("div.hidden").each(function(){
-          var type = $(this).data("parent");
-           if(type == filter){
-               $(this).toggleClass("active");
-           }
-       });
-   });
+$(function () {
+    $("a.dropdown").click(function () {
+        var filter = $(this).data("filter");
+
+        $("div.hidden").each(function () {
+            var type = $(this).data("parent");
+            if (type == filter) {
+                $(this).toggleClass("active");
+            }
+        });
+    });
+
+    $(document).on('click', '.report-head', function () {
+        $(this).next().slideToggle();
+
+    });
+
 });
