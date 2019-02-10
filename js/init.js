f61e5c00f99bcd502251a03b7c05397808694627
Unknown <tristan@elantris.com>
2019-02-09 21:36:23 -0800

    Homepage Update and Quiz WIP

---
+++ js/init.js
@@ -0,0 +1,132 @@
+var questions = [
+    "",
+    // Question 1
+    "I am afraid that I’ll be nervous during my first time, and do not know what to expect.",
+
+    // Question 2
+    "Sexual activity to me is:",
+
+    // Question 3
+    "The thought of blood, or other bodily fluids (eg. excrement, vaginal discharge) make me uneasy.",
+
+    // Question 4
+    "My arousal during sex can be described as:",
+
+    // Question 5
+    "I would consider the most important part of sex:",
+
+    // Question 6
+    "I am aware of the risk of pregnancy, and I am aware of the number of birth control options available to both men and women.",
+
+    // Question 7
+    "I believe that “pulling out” (pulling out of the vagina before ejactulation to prevent pregnacy) is a reliable method of birth control",
+
+    // Question 8
+    "I am aware of the risk of STDs, and I know where I can go to get tested for STDs or to receive sexual health resources",
+
+    // Question 9
+    "I feel that if my partner does not orgasm, I am bad at sex.",
+
+    // Question 10
+    "I believe that sex hurting for the first time, or first few times (for myself, or my partner) is normal."
+];
+
+
+var choices = [
+    [{}],
+    //    Question 1
+    [{
+        score: 0,
+        choice: "True"
+    }, {
+        score: 1,
+        choice: "False"
+    }],
+    //    Question 2
+    [{
+        score: 0,
+        choice: "Insertion only"
+    }, {
+        score: 1,
+        choice: "Oral and insertion"
+    }, {
+        score: 2,
+        choice: "All encompassing of intimate, physical activities"
+    }],
+    //    Question 3
+   [{
+        score: 0,
+        choice: "True"
+    }, {
+        score: 1,
+        choice: "False"
+    }],
+    // Question 4 
+    [{
+        score: 0,
+        choice: "I have a hard time staying aroused, or focused during sex"
+    }, {
+        score: 1,
+        choice: "Even though I feel aroused, I am unable to sustain, or get an erection"
+    }, {
+        score: 2,
+        choice: "My arousal level is high -- I ejaculate and / or orgrasm before my parter satisfied."
+    }],
+    // Question 5
+    [{
+        score: 0,
+        choice: "My own pleasure"
+    }, {
+        score: 1,
+        choice: "My partner's pleasure"
+    }, {
+        score: 2,
+        choice: "Both A and B"
+    }],
+    // Question 6
+    [{
+        score: 0,
+        choice: "True"
+    }, {
+        score: 1,
+        choice: "Only male options"
+    }, {
+        score: 2,
+        choice: "Only female options"
+    }, {
+        score: 3,
+        choice: "Unsure"
+    }],
+    // Question 7
+    [{
+        score: 0,
+        choice: "True"
+    }, {
+        score: 1,
+        choice: "False"
+    }],
+    // Question 8
+    [{
+        score: 0,
+        choice: "True"
+    }, {
+        score: 1,
+        choice: "False"
+    }],
+    // Question 9
+    [{
+        score: 0,
+        choice: "True"
+    }, {
+        score: 1,
+        choice: "False"
+    }],
+    // Question 10
+    [{
+        score: 0,
+        choice: "True"
+    }, {
+        score: 1,
+        choice: "False"
+    }]
+]
---
+++ js/quiz.js
@@ -0,0 +1,145 @@
+var pathID = 1;
+
+Vue.component('path-head', {
+    props: ['headers'],
+    template: '<h6>{{headers.text}}</h6>'
+})
+
+Vue.component('choice-head', {
+    props: ['choice'],
+    template: `<div class="choice" v-on:click="$emit('update-path')">{{choice}}</div>`
+})
+
+Vue.component('feedback', {
+    props: ['status'],
+    template: `<div id="feedbackitem" class="feedback"><slot></slot></div>`
+})
+
+Vue.component('choice-body', {
+    props: ['choice', 'link', 'status', 'image', 'ivideo'],
+
+    template: `<div class="choice" v-on:click="emitLink"><p class='title'>{{choice}}</p></div>`,
+    methods: {
+        emitLink: function () {
+            this.$emit('update-path', [this.link])
+        }
+    }
+})
+
+
+var vm = new Vue({
+    el: "#app",
+    data: {
+        prompt: questions[1],
+        feedbackStatus: "",
+        globalurl: 0,
+        vidSrc: "startVid",
+        headerList: [
+            {
+                id: 0,
+                text: "Header 1"
+            },
+            {
+                id: 1,
+                text: "Header 2"
+            },
+            {
+                id: 2,
+                text: "Header 3"
+            }
+        ],
+        choiceList2: [
+            {
+                option: choices[pathID][0].choice,
+                link: choices[pathID][0].score
+            },
+            {
+                option: choices[pathID][1].choice,
+                link: choices[pathID][1].score,
+            }
+//            ,
+//            {
+//                option: choices[pathID][2].choice,
+//                link: choices[pathID][2].score,
+//            }
+//            },
+//            {
+//                option: pathAnswers[pathID][2].choice,
+//                link: pathAnswers[pathID][2].link,
+//                status: pathAnswers[pathID][2].status
+//            }
+        ]
+    },
+    methods: {
+        updateMe: function (data) {
+
+
+
+            //Assign emitted data to variables
+            var url = data[0];
+            var status = "status";
+            var videodata = "videodata";
+            
+            pathID++;
+
+//            console.log(url + status + videodata)
+
+//
+//            console.log("update called <" + url + status + ">");
+
+            //DEBUG
+            console.log("url: " + url);
+//            console.log("status: " + status);
+//            console.log("video: " + videodata);
+
+
+
+            //Update choices for next question while video is playing in the front
+            window.setTimeout(function () {
+
+            vm.choiceList2 = [
+                {
+                        option: choices[pathID][0].choice,
+                        link: choices[pathID][0].score
+                },
+                {
+                        option: choices[pathID][1].choice,
+                        link: choices[pathID][1].score
+                }
+            ]
+
+            }, 400);
+
+
+
+
+
+
+
+            console.log("videodata " + videodata + " was updated")
+
+            //Update the message
+            //            this.message = pathHeaders[pathID];
+            //            this.choiceList = pathAnswers[pathID];
+
+
+
+
+        }
+    }
+})
+
+window.addEventListener("load", function (event) {
+
+    var scene = document.getElementById("scene");
+    var feedbackitem = document.getElementById("feedbackitem");
+    var seeker = document.getElementById("seekbar");
+    var overlay = document.getElementById("overlay");
+    var note = document.getElementById("notepad-overlay");
+    var cname = document.getElementById("character-name");
+
+
+
+
+    //do work
+});
